8719aefee52a472be4a8e32083b2bf9ad83f2d05
jeifar <gattes@tutamail.com>
2024-06-22 11:43:40 +0100

    Adding DEV branch

---
+++ constants.py
@@ -0,0 +1,6 @@
+USER_AGENT = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0"
+)
+LOGIN_URL = "https://www.noip.com/login"
+HOST_URL = "https://my.noip.com/dynamic-dns"
+SCREENSHOTS_PATH = "screenshots"
